9ace55313a1988d9da5ccec16d5c5408c8be164e
Yiju Wei <weiyiju@live.cn>
2017-11-30 00:39:03 +1100

    init

---
+++ log_probability.py
@@ -0,0 +1,12 @@
+from decimal import *
+import sys
+num_faces = long(sys.argv[1])
+from math import log, exp
+log_sum = 0.0
+i = 1
+while i < num_faces:
+	log_sum += log(i)
+	i += 1
+log_p = log_sum - num_faces * log(num_faces)
+p = Decimal(log_p).exp()#exp(log_p)
+print p
